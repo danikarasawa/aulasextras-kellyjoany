40b8eee82bd48d9beb7efaf285caf77bee66a46b
danikarasawa <danikarasawa@gmail.com>
2019-11-02 15:50:00 -0300

    new commit

--- aulaExtra/aula1.js
+++ aulaExtra/aula1.js
@@ -22,14 +22,14 @@ const students = [
 console.log(students[3]); //name: "Sarah", age:20
 console.log(students[3].name); //Sarah
 
-students.push({name: "Steve", age: 25});
+students.push({ name: "Steve", age: 25 });
 
-const bob = {name: "Bob", age: 21};
+const bob = { name: "Bob", age: 21 };
 students.push(bob);
 
 const twoD = [
-["Bob", "Susy", "Ted"]
-["Lilly", "Sarah", "Bill"]
+    ["Bob", "Susy", "Ted"]
+    ["Lilly", "Sarah", "Bill"]
 ];
 
 console.log(twoD[1]); //['Lilly', 'Sarah', 'Bill']
@@ -37,4 +37,30 @@ console.log(twoD[1][0]); // 'Lilly'
 console.log(twoD[0[0]]); //'Bob'
 console.log(twoD[0[3]]); // undefined
 console.log(twoD[3[0]]); //TypeError: Cannot read property '0' of undefined
+
+//OBJETO DENTRO DE OBJETO 
+
+const classRoom = {
+    teacher: {
+        firstName: 'Greg',
+        lastName: 'Dach',
+        age: 38,
+        address: {
+            street: "3085 Kelton Knolls",
+            city: "Aldaside",
+            state: "Maryland",
+            number: 300
+        }
+    }
+};
+
+console.log(classRoom.teacher.firstName); // 'Greg'
+console.log(classeRoom.teacher.age); // 38
+console.log(classRoom.lastName); // undefined
+console.log(classRoom.teacher.address.city); // 'Aldaside' ISSO É CHAMADO DE CAMINHO ABSOLUTO
+
+const teste = classRoom.teacher // teacher: {}
+const novoTeste = teste.address // teacher.address: {}
+const resultado = novoTeste.number // 300
+// ESSAS CONSTANTES DE QUEBRA DEIXAM O CÓDIGO MAIS ELEGANTE!!!  
 
